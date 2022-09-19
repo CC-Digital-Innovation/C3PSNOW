88c36117d65451723d76bc9778e4ad8b0a2e6715
Jonny Le <jonnyle2@gmail.com>
2022-09-19 15:37:26 -0500

    conventional quotes

--- c3api.py
+++ c3api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-@app.post("/sendOrder/")
+@app.post('/sendOrder/')
 async def sendOrder(Token: str = Form(...),
                      name: str = Form(...),
                      cocktail: str = Form(...),
@@ -24,43 +24,40 @@ async def sendOrder(Token: str = Form(...),
             #USe SNOW API to screate snow incident: Done
 
             location_Map = {
-                "1"  : "Hole 1",
-                "2"  : "Hole 2",
-                "3"  : "Hole 3",
-                "4"  : "Hole 4",
-                "5"  : "Hole 5",
-                "6"  : "Hole 6",
-                "7"  : "Hole 7",
-                "8"  : "Hole 8",
-                "9"  : "Hole 9",
-                "10" : "Hole 10",
-                "11" : "Hole 11",
-                "12" : "Hole 12",
-                "13" : "Hole 13",
-                "14" : "Hole 14",
-                "15" : "Hole 15",
-                "16" : "Hole 16",
-                "17" : "Hole 17",
-                "18" : "Hole 18"
+                '1'  : 'Hole 1',
+                '2'  : 'Hole 2',
+                '3'  : 'Hole 3',
+                '4'  : 'Hole 4',
+                '5'  : 'Hole 5',
+                '6'  : 'Hole 6',
+                '7'  : 'Hole 7',
+                '8'  : 'Hole 8',
+                '9'  : 'Hole 9',
+                '10' : 'Hole 10',
+                '11' : 'Hole 11',
+                '12' : 'Hole 12',
+                '13' : 'Hole 13',
+                '14' : 'Hole 14',
+                '15' : 'Hole 15',
+                '16' : 'Hole 16',
+                '17' : 'Hole 17',
+                '18' : 'Hole 18'
             }
 
-            inc = {"u_drink_requester":f"{name}",
-                "u_drink":f"{cocktail}",
-                "location":f"{location_Map[hole_Num]}",
-                "u_quantity":f"{quantity}",
-                "urgency":f"{severity}",
-                "description" : f"{spec_Instruct}",
-                "short_description":f"{name} ordered {quantity} {cocktail}(s) at {location_Map[hole_Num]}"}
+            inc = {'u_drink_requester':f'{name}',
+                'u_drink':f'{cocktail}',
+                'location':f'{location_Map[hole_Num]}',
+                'u_quantity':f'{quantity}',
+                'urgency':f'{severity}',
+                'description' : f'{spec_Instruct}',
+                'short_description':f'{name} ordered {quantity} {cocktail}(s) at {location_Map[hole_Num]}'}
 
             url = config.get('endpoints', 'incurl')
             user = config.get('creds', 'user')
             passw = config.get('creds', 'pass')
-            headers = {"Content-Type":"application/json","Accept":"application/json"}
+            headers = {'Content-Type':'application/json','Accept':'application/json'}
             resp = requests.post(url, auth=(user,passw), headers = headers, data = json.dumps(inc))
             #Return status ie order failed/created
             #Use OpsGenie API to create OPsGenie alert
         else:
-            print("You are not authorized to use this service")
-
-    
-    
+            print('You are not authorized to use this service')
