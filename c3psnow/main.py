af28639ec55f475b5b9f5a9e5d2a8598a87abcb4
Jonny Le <jonnyle2@gmail.com>
2022-09-27 13:12:40 -0500

    undo root path for dev ingress

--- c3psnow/main.py
+++ c3psnow/main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == '__main__':
-    uvicorn.run('c3api:app', host='0.0.0.0', port = 8000)
+    uvicorn.run('c3api:app', host='0.0.0.0', port = 8000, root_path='/c3psnow-dev')
