225738883d9f27e565857b0cced063089f40310d
Ben Verley <48893829+Bverley92@users.noreply.github.com>
2022-10-04 16:27:07 -0700

    Added today filter to   queue pull

--- c3psnow/c3api.py
+++ c3psnow/c3api.py
@@ -206,6 +206,7 @@ fields = ['sys_created_on', 'u_drink_requester', 'urgency', 'state',
 @app.get('/queue', dependencies=[Depends(authorize)])
 async def get_queue(state: State = None, offset: int = 0, limit: int = 10):
     today = date.today().strftime("%Y-%m-%d")
+    print(date.today())
     incident_resource = snow_client.resource('/table/incident')
     params = {
         'sysparm_display_value': True,
